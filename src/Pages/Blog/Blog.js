26c47bcd7cab073027efeca9ecb88276232f8e83
Poly Akhtar <108450477+polyakhtar@users.noreply.github.com>
2023-08-21 05:26:23 +0000

    blog file correction

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blog = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
-            <h1 className='text-5xl font-semibold text-white '>Coming Soon....</h1>
+            <h1 className='text-5xl font-semibold text-white '>Coming Soon..</h1>
         </div>
     );
 };
